f75fa49811bf441c7b12c6907c7e708786efcc9e
Christoffer Langstrøm Knudsen <christoflk@uia.no>
2022-11-03 14:54:00 +0100

    Test Push

---
+++ ResturantBooking/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Resturant_Booking.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
