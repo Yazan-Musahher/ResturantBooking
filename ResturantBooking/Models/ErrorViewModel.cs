7f3509a7e08cabc299f925a1745962b66c12b4f2
kriger452 <117357535+kriger452@users.noreply.github.com>
2022-11-03 19:48:01 +0100

    Delete ResturantBooking directory

--- ResturantBooking/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Resturant_Booking.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
