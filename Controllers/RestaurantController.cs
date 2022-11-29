c7f2a3948ebe2ffcd8cb5eea33569416f8de2e1c
aleksandsk <aleksandsk@uia.no>
2022-11-29 16:12:31 +0100

    Made it so that only people who have access to a restaurant, or admins can see their restaurantpage

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -1,20 +1,28 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Net;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Resturant_Booking.Data;
 using Resturant_Booking.Models;
+
+using Microsoft.AspNetCore.Identity;
+
+
 
 namespace Resturant_Booking.Controllers;
 
 public class RestaurantController : Controller
 {
     private ApplicationDbContext _db;
+    private UserManager<ApplicationUser> _um;
     
-    public RestaurantController(ApplicationDbContext db)
+    public RestaurantController(ApplicationDbContext db, UserManager<ApplicationUser> um)
     {
         _db = db;
+        _um = um;
     }
-    
+
     // GET
     [HttpGet]
     [Authorize]
@@ -27,13 +35,17 @@ public class RestaurantController : Controller
         {
             restaurantId = Int32.Parse(restaurantIdString);
         }
-
-        //if(routeId != applicationUser.restaurant)
-        // return ;
+        
+        // Check that user has access to restaurant
+        var applicationUser = _um.GetUserAsync(User).Result;
+        if (restaurantId != applicationUser.RestaurantId && !User.IsInRole("Admin"))
+        {
+            return RedirectToRoute("default", 
+                new { controller = "Restaurant", action = "Index", id = applicationUser.RestaurantId.ToString() });
+        }
         
         
-        // Get restaurant from db and pass to view
-
+        // Get all reservations to restaurant
         var reservations = _db.Reservations
             .Include(i => i.Table.Restaurant)
             .Include(i => i.User)
