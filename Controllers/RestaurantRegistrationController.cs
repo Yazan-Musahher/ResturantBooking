39c9890edccda6f3c8a27527f091eafb8de7bec5
Christoffer Langstrøm Knudsen <christoflk@uia.no>
2022-11-30 23:33:45 +0100

    Added restaurant register view and home screen with a small problem:)

---
+++ Controllers/RestaurantRegistrationController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Resturant_Booking.Data;
+using Resturant_Booking.Models;
+
+namespace Resturant_Booking.Controllers;
+
+public class RestaurantRegistrationController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public RestaurantRegistrationController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(new Restaurant());
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Index(Restaurant restaurant)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(restaurant);
+        }
+
+        _db.Restaurants.Add(restaurant);
+        _db.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+}
