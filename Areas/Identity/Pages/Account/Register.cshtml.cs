0b9d6133dc7c8e3c2feacb9f86e23b8830bf3273
Mohamad Yazan Musahher <mohamed.musahher@gmail.com>
2022-12-12 10:29:15 +0100

    har satt opp API for å hente alle brukere fra db

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -69,6 +69,7 @@ namespace Resturant_Booking.Areas.Identity.Pages.Account
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        ///
         public class InputModel
         {
             /// <summary>
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+ window.addEventListener("scroll", function ()
+{
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scroll)
+})
+
+    
