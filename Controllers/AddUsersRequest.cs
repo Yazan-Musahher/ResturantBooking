c31a66e6885ef09ea4d7f2627f592448fe287ba9
Mohamad Yazan Musahher <mohamed.musahher@gmail.com>
2022-12-12 19:18:39 +0100

    fikset lit på hjemme siden

---
+++ Controllers/AddUsersRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Resturant_Booking.Controllers;
+
+public class AddUsersRequest
+{
+     public string Email { get; set; }
+     
+     public string Name { get; set; }
+     
+     public string PasswordHash { get; set; }
+
+
+}
--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.Build.Framework;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace Resturant_Booking.Models;
 
 public class ApplicationUser : IdentityUser
 {
-    [Required]
+    [Microsoft.Build.Framework.Required]
     public string Name { get; set; } = String.Empty;
+    
+    [StringLength(20)]
+    public override string Email { get; set; }
+    
+    public override string PasswordHash { get; set; }
+    
+    
     
     
     // Foreign key
