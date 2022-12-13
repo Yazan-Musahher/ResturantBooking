17ac25ad5688a71c988ea73b0790eeda7b171991
Christoffer Langstrøm Knudsen <christoflk@uia.no>
2022-12-13 15:21:18 +0100

    Add paypal to this one.

---
+++ Data/MessageService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Resturant_Booking.Models;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace TwilioTest.Data;
+
+// Twilio documentation
+// https://www.twilio.com/docs/sms/quickstart/csharp-dotnet-framework
+public class MessageService
+{
+    public MessageService()
+    {
+        
+    }
+
+    public void SendMessage(Restaurant restaurant, Reservation reservation)
+    {
+        // Initalize Twilio client
+        TwilioClient.Init(accountSid, authToken);
+
+        // Message which gets sent to the customer
+        var message = MessageResource.Create(
+            body: $"You have booked a table at {restaurant.Name}. " +
+                  $"Looking forward to see you at {reservation.Time}.",
+            from: new Twilio.Types.PhoneNumber(phoneNumberFrom),
+            to: new Twilio.Types.PhoneNumber(phoneNumberTo)
+        );
+        
+        Console.WriteLine(message.Sid);
+    }
+    
+    private string accountSid = "ACae555bdac3267303504489e9accf25b9";
+    private string authToken = "3c07bfc67f561ee7da5d70ede89fa3ae";
+    private string phoneNumberFrom = "+12674632427";
+    private string phoneNumberTo = "+4747752193";
+}
