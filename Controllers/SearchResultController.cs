7d18924b31042d918c5c9cb80eb1c79a17e9bac7
Christoffer Langstrøm Knudsen <christoflk@uia.no>
2022-12-13 11:54:53 +0100

    Search bar

---
+++ Controllers/SearchResultController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Resturant_Booking.Data;
+using Resturant_Booking.Models;
+
+namespace Resturant_Booking.Controllers;
+
+public class SearchResultController : Controller
+{
+    private readonly ApplicationDbContext _db;
+    
+    public SearchResultController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    
+    [HttpGet]
+    // Documentation from Microsoft on search bar
+    // https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/search?view=aspnetcore-7.0
+    public async Task<IActionResult> Index(string searchCity)
+    {
+        // Checks if database does not have any restaurants
+        if (_db.Restaurants == null)
+        {
+            return Problem("There are no restaurants in the database.");
+        }
+
+        // Gets restaurants from the database
+        var restaurants = from r in _db.Restaurants
+            select r;
+
+        //Chekcs if search string is empty
+        if (!String.IsNullOrEmpty(searchCity))
+        {
+            // Finds restaurants which has the searched string as substring
+            restaurants = restaurants.Where(res => res.Address.Contains(searchCity));
+        }
+
+        return View(await restaurants.ToListAsync());
+    }
+}
